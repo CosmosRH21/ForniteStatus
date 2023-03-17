bf44792b915d0805596477111a20e7e562b3f61f
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2023-03-17 01:06:57 -0600

    api fornite

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from "./Layouts/Login";
+import Home from "./Layouts/Home";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layouts/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [seasonLevel, setSeasonLevel] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://fortnite-api.com/v2/stats/br/v2", {
+        params: {
+          name: "ALITARC05",
+        },
+        headers: {
+            'Authorization': 'e0b93223-19ba-406c-840f-4793374fd8ee'
+          }
+      })
+      .then((response) => {
+        setSeasonLevel(response.data.data.battlePass.level);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>HOME</h1>
+      {seasonLevel ? (
+        <p>Nivel actual de la temporada: {seasonLevel}</p>
+      ) : (
+        <p>Cargando...</p>
+      )}
+    </div>
+  );
+}

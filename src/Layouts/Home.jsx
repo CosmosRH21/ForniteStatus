c35b6cf4febec285ac04ba3f0afdd71b3fb0550f
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2023-03-18 01:23:31 -0600

    Update Home.jsx

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -2,37 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [seasonLevel, setSeasonLevel] = useState(null);
+  const [seasonLevels, setSeasonLevels] = useState({});
+  const names = ["ALITARC05", "DullestYapper9", "JussefoX"];
 
   useEffect(() => {
-    axios
-      .get("https://fortnite-api.com/v2/stats/br/v2", {
-        params: {
-        //   name: "ALITARC05",
-        //   name: "DullestYapper9",
-          name: "JussefoX",
-        },
-        headers: {
-            'Authorization': 'e0b93223-19ba-406c-840f-4793374fd8ee'
-          }
-      })
-      .then((response) => {
-        setSeasonLevel(response.data.data.battlePass.level);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const fetchSeasonLevels = async () => {
+      const newSeasonLevels = {};
+      for (const name of names) {
+        try {
+          const response = await axios.get("https://fortnite-api.com/v2/stats/br/v2", {
+            params: {
+              name: name,
+            },
+            headers: {
+              Authorization: "e0b93223-19ba-406c-840f-4793374fd8ee",
+            },
+          });
+          newSeasonLevels[name] = response.data.data.battlePass.level;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      setSeasonLevels(newSeasonLevels);
+    };
+    fetchSeasonLevels();
   }, []);
 
   return (
     <div>
       <h1>HOME</h1>
-      {seasonLevel ? (
-        <p>Nivel actual de la temporada: {seasonLevel}</p>
+      {Object.keys(seasonLevels).length > 0 ? (
+        <div>
+          {names.map((name, index) => (
+            <p key={index}>
+              Nivel actual de la temporada de {name}: {seasonLevels[name]}
+            </p>
+          ))}
+        </div>
       ) : (
         <p>Cargando...</p>
       )}
     </div>
   );
 }
+
+
